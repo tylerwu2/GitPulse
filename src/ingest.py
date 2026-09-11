be342fee701c7d480c8ecad8cb731f15738e7793
Tyler Wu <tyler_wu@berkeley.edu>
2026-09-10 21:10:09 -0700

    added files for repo scaffold and updated src

--- src/ingest.py
+++ src/ingest.py
@@ -1,7 +1,7 @@
 import requests
 
 def fetch_data(url: str):
-    response = requests.get(url)
+    response = requests.get(url, timeout=30)
     # automatically throws HTTPException if error occurs
     response.raise_for_status()
     # convert response to json output
--- src/transform.py
+++ src/transform.py
@@ -1,2 +1,8 @@
+import pandas as pd
+
 def transform_data():
     return None
+
+
+def convert_to_df(data):
+    return pd.DataFrame(data)
