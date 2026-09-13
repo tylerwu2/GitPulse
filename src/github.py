d3a3fdb15bc74a6d5dbe82c30aee923ff7147423
Tyler Wu <tyler_wu@berkeley.edu>
2026-09-12 23:08:39 -0700

    added GitHub specific files, helper func for getting pull requests data

---
+++ src/github.py
@@ -0,0 +1,17 @@
+# script to connect to GitHub API client
+
+import requests
+
+def get_pull_requests(owner, repo, page=1):
+    url = f"https://api.github.com/repos/{owner}/{repo}/pulls"
+
+    params = {
+        "state" : "all",
+        "per_page" : 100,
+        "page" : 1
+    }
+
+    response = requests.get(url, params = params)
+    response.raise_for_status()
+    data = response.json()
+    return data
--- src/load.py
+++ src/load.py
@@ -4,4 +4,3 @@ def load_data(df, database_url, table_name):
     engine = create_engine(database_url)
 
     df.to_sql(table_name, engine, if_exists="append", index=False)
-    
